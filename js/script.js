6ae584fbd983fa1c0877d88972a29bf934ae3d70
Fabio9494 <fabio.casoria94@gmail.com>
2023-11-17 14:06:39 +0100

    password creation

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let nome_utente= prompt ("Inserisci nome utente");
+console.log(nome_utente)
+
+let cognome_utente= prompt ("Inserisci cognome utente");
+console.log(cognome_utente)
+
+let colore_preferito= prompt ("Inserisci colore preferito");
+console.log(colore_preferito)
+
+document.getElementById("password-poco-sicura").innerHTML=`La tua password poco sicura è: ${nome_utente}${cognome_utente}${colore_preferito}`;
