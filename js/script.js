ea6bf2759cda22145d2c00383ad29d4d21c04bd4
Fabio9494 <fabio.casoria94@gmail.com>
2023-11-17 17:05:47 +0100

    improved code syntax

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-let nome_utente= prompt ("Inserisci nome utente");
+let nome_utente = prompt("Inserisci nome utente");
 console.log(nome_utente)
 
-let cognome_utente= prompt ("Inserisci cognome utente");
+let cognome_utente = prompt("Inserisci cognome utente");
 console.log(cognome_utente)
 
-let colore_preferito= prompt ("Inserisci colore preferito");
+let colore_preferito = prompt("Inserisci colore preferito");
 console.log(colore_preferito)
 
-document.getElementById("password-poco-sicura").innerHTML=`La tua password poco sicura è: ${nome_utente}${cognome_utente}${colore_preferito}`;
+document.getElementById("password-poco-sicura").innerHTML = `La tua password poco sicura è: ${nome_utente}${cognome_utente}${colore_preferito}`;
